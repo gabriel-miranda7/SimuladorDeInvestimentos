6302062ee420d50c05743b58e91f1f6f1477713c
Gabriel da Silveira Miranda <48613988+gabriel-miranda7@users.noreply.github.com>
2023-05-02 20:02:11 -0300

    Update CalculadoraDeRendimentos.py

--- CalculadoraDeRendimentos.py
+++ CalculadoraDeRendimentos.py
@@ -71,6 +71,35 @@ def selic (valor, tempoi, tempof): #Trabalha o investimento no tesouro Selic.
     sleep(1)
     rendimento = valor * ((1 + 0.1375/252) ** intervalo - 1) #Calcula o rendimento.
     print(f'O seu rendimento líquido será de R${round(rendimento, 2)} e o montante final será de R${round(valor+rendimento, 2)}')
+
+def cdbprefixado (valor, tempoi, tempof): #Trabalha o investimento no tesouro Selic.
+    from time import sleep
+    intervalo = 0
+    system('cls' if name == 'nt' else 'clear')
+    print('Bem Vindo(a) á calculadora de investimentos do CDB Pré-Fixado.')
+    sleep(1)
+    taxa = input('Insira aqui a taxa de rendimento em % ao ano: ')
+    lista = list()
+    for i in range(0, len(taxa)): #Retira da taxa qualquer possível entrada que não seja numérica
+        if taxa[i].isnumeric() or taxa[i] == '.' or taxa[i] == ',':
+            if taxa[i] == ',': #Substitui uma possível , digitada na taxa por um .
+                lista.append('.')
+            else:
+                lista.append(taxa[i])
+    taxa = float(''.join(lista)) #Recebe a taxa em um valor float.
+    valor = input('Insira aqui o valor inicial (Primeiro Mês) que deseja investir. R$ ') #Recebe o valor de entrada do investimento
+    valor = valor.replace(',', '.') #Substitui uma posível vírgula digitada pelo usuário por um ponto
+    valor = float(valor)
+    system('cls' if name == 'nt' else 'clear')
+    print('Muito bem! Agora vamos definir o tempo de investimento')
+    tempoi = verificadordedatainicial(tempoi)#Ativa a função de verificação do formato de data inicial.
+    tempof = verificadordedatafinal(tempof, tempoi) #Ativa a função de verificação do formato de data final.
+    intervalo = diasuteis(tempoi, tempof)  #Ativa a função dias úteis.
+    system('cls' if name == 'nt' else 'clear')
+    print(f'O seu dinheiro estará investido em um intevalo de {intervalo} dias úteis. [exceto feriados.]')
+    sleep(1)
+    rendimento = valor * ((1 + (taxa/100)/252) ** intervalo - 1) #Calcula o rendimento.
+    print(f'O seu rendimento líquido será de R${round(rendimento, 2)} e o montante final será de R${round(valor+rendimento, 2)}')
     
 
 def diasuteis(datai, dataf): #Função que vai selecionar apenas os dias úteis(Dias de Rendimento) no intervalo escolhido.
@@ -83,4 +112,6 @@ def diasuteis(datai, dataf): #Função que vai selecionar apenas os dias úteis(
 escolha = menu(escolha)
 if escolha == 1:
     selic(valor, tempoi, tempof)
+elif escolha == 2:
+    cdbprefixado(valor, tempoi, tempof)
 
